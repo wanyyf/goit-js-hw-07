a9a75453d3c7ad6f2e44679bf72773434051280e
wanyyf <vanyaposta@gmail.com>
2022-07-02 18:21:53 +0300

    all

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+// Change code below this line
+const galleryEl = document.querySelector(".gallery")
+
+
+const creatorItemDiv = ({preview,original,description}={}) => {
+    return   `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+}
+
+const createItem = () => { 
+ return galleryItems.map(el => {
+ return creatorItemDiv(el)
+}).join("")
+}
+galleryEl.insertAdjacentHTML('beforeend',createItem(galleryItems))
+
+
+const onLinkClick = (evt) => {
+  
+    if (evt.target.nodeName !== "IMG") return
+    const getSrcImg = evt.target;
+    evt.preventDefault()
+    const backdrop = basicLightbox.create(`
+		<img width="1400" height="900" src="${getSrcImg.dataset.source}">
+	`).show(() => {
+        window.addEventListener("keydown", (event) => {
+            
+            if (event.code === "Escape"){
+                backdrop.close();
+            }
+        })
+    })
+   
+}
+galleryEl.addEventListener("click", onLinkClick)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery")
+
+
+const creatorItemDiv = ({preview,original,description}={}) => {
+    return   `
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  `
+}
+
+const createItem = () => { 
+ return galleryItems.map(el => {
+ return creatorItemDiv(el)
+}).join("")
+}
+galleryEl.insertAdjacentHTML('beforeend',createItem(galleryItems))
+
+ new SimpleLightbox('.gallery a'),{
+     captionsData:"alt",
+    captionDelay: 250,
+    enableKeyboard: "true",
+ };
